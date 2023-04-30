21897ec6ce0b5a8fbd2c979ff545526755b259e1
Makoto kamimura <68632305+makoto-kamimura@users.noreply.github.com>
2023-04-30 17:15:22 +0900

    add plotly

--- app/analysis/views.py
+++ app/analysis/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import plotly.graph_objects as go
+import pandas
 
-# view_main
 def main(request):
-    return render(request, 'analysis/main.html', {})
+    # データ元
+   df = pandas.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+   fig = go.Figure(data=[go.Candlestick(
+       x=df['Date'],
+       open=df['AAPL.Open'],
+       high=df['AAPL.High'],
+       low=df['AAPL.Low'],
+       close=df['AAPL.Close']
+       )])
+
+   plot_fig = fig.to_html(fig, include_plotlyjs=False)
+   return render(request, "analysis/main.html", {"graph": plot_fig})
