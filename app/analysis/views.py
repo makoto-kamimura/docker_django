b1d2868b237788703e4d925a13b7858b33af2b88
Makoto kamimura <68632305+makoto-kamimura@users.noreply.github.com>
2023-11-03 11:59:42 +0900

    update csv-data

--- app/analysis/views.py
+++ app/analysis/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import plotly.graph_objects as go
 import pandas
+import pandas_datareader.data  as pdr
+import datetime
 
 def main(request):
     # データ元
-   df = pandas.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+   df = pandas.read_csv('analysis/data/finance-charts-apple.csv')
 
    fig = go.Figure(data=[go.Candlestick(
        x=df['Date'],
@@ -16,3 +18,46 @@ def main(request):
 
    plot_fig = fig.to_html(fig, include_plotlyjs=False)
    return render(request, "analysis/main.html", {"graph": plot_fig})
+
+# Exchange
+def real_time_exchange_rate(pair):
+    # 現在時刻
+    dt_now = datetime.datetime.now()
+    today = dt_now.strftime('%Y-%m-%d')
+    start, end = today, today
+    
+    # 為替pairを所定の形に変更
+    code = f'{pair}=X'
+
+    # dataの取得
+    data = pdr.get_data_yahoo(code, end, start)
+
+    # 最終日（今日）の終値（リアルタイム値）を返す
+    return data['Close'][-1]
+
+# USDJPY = real_time_exchange_rate('USDJPY')
+# print(USDJPY)
+
+# Stock
+def real_time_stock_price(ticker, source='yahoo'):
+    # 現在時刻
+    dt_now = datetime.datetime.now()
+    today = dt_now.strftime('%Y-%m-%d')
+    start, end = today, today
+    
+    # 株価の取得
+    data = pdr.DataReader(ticker, source, end, start)
+    
+    # 最終日（今日）のdataを抽出
+    data = data.tail(1)
+
+    # indexをtickerシンボルに書き換える
+    data = data.rename(index={data.index[0]: ticker})
+    data.index.name='Ticker'
+
+    # 余分な特徴量を削除する
+    data = data.drop(['High', 'Low', 'Open', 'Close', 'Volume'], axis=1)
+    return data
+
+# data = real_time_stock_price('AAPL')
+# print(data)
